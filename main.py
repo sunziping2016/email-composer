0820605562e70943f977a68b9ea4f040cccb5961
Ziping Sun <me@szp.io>
2023-02-13 17:53:58 +0800

    feat: refactor all to templates

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import argparse
 import base64
-import csv
 import glob
 import io
 import json
@@ -19,72 +18,65 @@ import pandas as pd
 import qrcode
 
 
-def generate_clients(contacts) -> str:
-    clients_config = ',\n'.join(contacts.apply(lambda contact: f'''\
-{{
-  "email": "{contact['email']}",
-  "id": "{contact['uuid']}",
-  "level": 0,
-  "alterId": 4
-}}''', axis=1))
-    return f"[{clients_config}]"
-
-
-def generate_vmess_url(config, id_: str) -> str:
-    v2_config = config['v2']
-    v2_config['id'] = id_
-    return 'vmess://' + base64.b64encode(json.dumps(v2_config).encode()).decode()
-
-
-def generate_uuid(namespace: uuid.UUID, email: str) -> uuid.UUID:
-    return uuid.uuid5(namespace, email)
-
-
 def load_config(filename: str):
     with open(filename) as f:
         return json.load(f)
 
 
-def load_contacts(filename: str, data: Optional[str] = None):
-    contacts = pd.read_csv(filename)
-    if data is not None:
-        for extra in glob.iglob(f'{data}/**/*.csv', recursive=True):
-            contacts = pd.merge(contacts, pd.read_csv(
-                extra), on='email', how='left')
+def load_contacts(args):
+    contacts = pd.read_csv(args.contacts)
+    for extra in glob.iglob(f'{args.data}/**/*.csv', recursive=True):
+        contacts = pd.merge(contacts, pd.read_csv(extra),
+                            on='email', how='left')
+    if args.email:
+        contacts = contacts[contacts['email'].isin(args.email)]
+    if args.tag:
+        contacts = contacts[contacts['tag'].isin(args.tag)]
     return contacts
 
 
-def run_init(args: argparse.Namespace) -> None:
+def create_env(args, config, contacts):
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader('.'), autoescape=jinja2.select_autoescape())
+    env.globals.update(config)
+    env.globals.update({
+        "contacts": [row.to_dict() for _, row in contacts.iterrows()],
+    })
+    env.filters.update({
+        'update': lambda x, y: {**x, **y},
+        'uuid5': lambda x, namespace: str(uuid.uuid5(uuid.UUID(namespace), x)),
+        'to_json': lambda x: json.dumps(x, separators=(',', ':')),
+        'to_base64': lambda x: base64.b64encode(x.encode()).decode(),
+        'dump_txt': lambda x: base64.b64encode(x.encode()).decode() + '.plain.txt',
+        'dump_qr': lambda x: (
+            qr_img := qrcode.make(x),
+            qr_img_bytes_io := io.BytesIO(),
+            qr_img.save(qr_img_bytes_io, format='PNG'),
+            base64.b64encode(qr_img_bytes_io.getvalue()
+                             ).decode() + '.image.png',
+        )[-1],
+    })
+    env.filters['txt_file'] = lambda x: env.filters['to_base64'](
+        x) + '.plain.txt'
+    return env
+
+
+def run_render(args: argparse.Namespace) -> None:
     config = load_config(args.config)
-    contacts = load_contacts(args.contacts)
-    namespace = uuid.UUID(config['namespace'])
-    contacts['uuid'] = contacts['email'].map(
-        lambda x: generate_uuid(namespace, x))
-    if args.write_contacts:
-        contacts.to_csv(args.contacts, index=False)
-    clients_config = generate_clients(contacts)
-    print(clients_config)
+    contacts = load_contacts(args)
+    env = create_env(args, config, contacts)
+    template = env.get_template(args.template)
+    print(template.render())
 
 
 def run_send(args: argparse.Namespace) -> None:
     config = load_config(args.config)
-    contacts = load_contacts(args.contacts, args.data)
-    if args.filter_email:
-        contacts = contacts[contacts['email'].isin(args.filter_email)]
-    if args.filter_tag:
-        contacts = contacts[contacts['tag'].isin(args.filter_tag)]
+    contacts = load_contacts(args)
+    env = create_env(args, config, contacts)
+    txt_template = env.get_template(args.template)
     email = config['smtp']
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader(
-        args.templates), autoescape=jinja2.select_autoescape())
-    txt_template = env.get_template(f'{args.template}/template.txt')
-    html_template = env.get_template(f'{args.template}/template.html')
     msgs: List[EmailMessage] = []
     for _, contact in contacts.iterrows():
-        with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-            print(contact)
-        reply = input('Resume (y/N): ')
-        if reply.lower() != 'y':
-            sys.exit(1)
         txt = frontmatter.loads(txt_template.render(contact.to_dict()))
         msg = EmailMessage()
         msg.set_charset('utf-8')
@@ -93,18 +85,23 @@ def run_send(args: argparse.Namespace) -> None:
         msg['To'] = (Address(contact['english_name'],
                      *contact['email'].split('@')),)
         msg.set_content(str(txt))
-        msg.add_alternative(html_template.render(
-            contact.to_dict()), subtype='html')
-        if txt.get('attach_vmess', False):
-            vmess = generate_vmess_url(config, contact['uuid'])
-            qr_img = qrcode.make(vmess)
-            qr_img_bytes_io = io.BytesIO()
-            qr_img.save(qr_img_bytes_io, format='PNG')
-            qr_img_bytes = qr_img_bytes_io.getvalue()
-            msg.add_attachment(vmess.encode(), maintype='text',
-                               subtype='plain', filename='vmess.txt')
-            msg.add_attachment(qr_img_bytes, maintype='image',
-                               subtype='png', filename='vmess.png')
+        print(' main:')
+        print('\n'.join(map(lambda x: '  > ' + x, str(txt).split('\n'))))
+        for filetype, path in txt.get('alternative', {}).items():
+            subtype = filetype.strip()
+            template = env.get_template(path)
+            content = template.render(contact.to_dict())
+            print(f'\n {filetype}:')
+            print('\n'.join(map(lambda x: '  > ' + x, content.split('\n'))))
+            msg.add_alternative(content, subtype=subtype)
+        for meta in txt.get('attach', []):
+            content, maintype, subtype = meta['content'].strip().split('.')
+            content = base64.b64decode(content.encode())
+            msg.add_attachment(content, maintype=maintype,
+                               subtype=subtype, filename=meta['name'])
+        reply = input(f'{contact.email} (y/N): ')
+        if reply.lower() != 'y':
+            sys.exit(1)
         msgs.append(msg)
         print()
     with smtplib.SMTP_SSL(email['server'], email.get('port', 465)) as smtp:
@@ -119,27 +116,23 @@ def main():
                              default='config.json', help='configuration file')
     main_parser.add_argument('--contacts', type=str,
                              default='contacts.csv', help='contacts file')
-    main_parser.add_argument('--templates', type=str,
-                             default='templates', help='directory to templates')
     main_parser.add_argument('--data', type=str,
                              default='data', help='additional data')
+    main_parser.add_argument(
+        '--email', action='append', default=[], help='filter emails')
+    main_parser.add_argument(
+        '--tag', action='append', default=[], help='filter tags')
     sub_parsers = main_parser.add_subparsers(
         title='command', help='available sub-commands')
     # init
-    parser_init = sub_parsers.add_parser('init', help='generate v2ray config')
+    parser_init = sub_parsers.add_parser('render', help='render template')
     parser_init.add_argument(
-        '--write_contacts', action='store_true', help='write back IDs to contacts file')
-    parser_init.set_defaults(func=run_init)
+        'template', help='the template used to render')
+    parser_init.set_defaults(func=run_render)
     # send
     parser_email = sub_parsers.add_parser('send', help='send emails')
     parser_email.add_argument(
         'template', help='the template used to compose email')
-    parser_email.add_argument(
-        '--filter_email', action='append', default=[], help='filter emails')
-    parser_email.add_argument(
-        '--filter_tag', action='append', default=[], help='filter tags')
-    # parser_email.add_argument(
-    #     '--attach', action='store_true', help='send attachment')
     parser_email.set_defaults(func=run_send)
 
     args = main_parser.parse_args()
